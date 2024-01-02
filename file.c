a7718e6220e6f1baf346a8c1e0d7cebb37eec27a
laiaya <118039423+ayaLi023@users.noreply.github.com>
2024-01-03 00:43:14 +0100

    les primitives de manipulation

--- file.c
+++ file.c
@@ -1,5 +1,66 @@
 #include<stdio.h>
 #include<stdlib.h>
+// definition de la structure d`un noeud 
+typedef struct noeud {
+    int info ;
+    struct noeud* suiv ;
+}noeud ;
+// definition d`une structure pour la file 
+typedef struct file {
+    noeud* tete ;
+    noeud* queue ;
+}file;
+// initialisation  d`une file vide 
+file initfile (){
+    file f ;
+    f.tete=NULL;
+    f.queue=NULL;
+    return f ;
+}
+// fonction pour verifier si la file est vide 
+int filevide (file f ){
+    return f.tete==NULL && f.queue==NULL ;
+}
+// fonction pour consulter la valeur en tete de file 
+int tetefile (file f){
+return f.tete ->info;
+}
+// fonction pour enfiler un element dans la file 
+void enfiler ( file *f , int x ){
+    // creation d`un nouveau noeud 
+    noeud* tmp = malloc (sizeof(noeud));
+    tmp->info = x;
+    tmp->suiv=NULL;
+    // cas ou la file est vide 
+    if (filevide(*f)==1)
+    {
+        f->tete=tmp;
+        f->queue=tmp;
+    }
+    else  // ajouter a la fin  de file 
+    {
+        f->queue->suiv=tmp;
+        f->queue=tmp;
+    }
+}
+//fonction pour difeler la file 
+void defiler ( file* f ,int x ){
+    noeud *tmp ;
+    // recuperer la valeur en tete de file 
+    x=f->tete->info;
+    tmp=f->tete;
+    if (f->tete==f->queue)
+    {
+        f->tete=NULL;
+        f->queue=NULL;
+    }
+    else {
+        // supression de noeud en tete 
+        f->tete=f->tete->suiv;
+    }
+    // liberation de la memoire du noeud de tete 
+    free(tmp);
+}
 int main (){
 printf (" utiliser les fonctions primitives pour votre projet\n ");
 return 0;
